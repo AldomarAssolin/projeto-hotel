80f075e22b0bfcca96383647e995b34524697e3c
AldomarAssolin <assolinaldomar@gmail.com>
2021-10-20 14:36:46 -0300

    aj09

--- js/login.js
+++ js/login.js
@@ -4,12 +4,12 @@ let register = document.querySelector('.register')
 const btn_register = document.querySelector('#btn_register')
 const btn_login = document.querySelector('#btn_login')
 
-btn_login.addEventListener('click', function(){
+btn_login.addEventListener('focus', function(){
     register.style.display = 'block'
     login.style.display = 'none'
 })
 
-btn_register.addEventListener('click', function(){
+btn_register.addEventListener('focus', function(){
     register.style.display = 'none'
     login.style.display = 'block'
 })
