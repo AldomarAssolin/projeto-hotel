71139b44d00f470c47754fbb4042e42ef07738e6
AldomarAssolin <assolinaldomar@gmail.com>
2021-10-20 14:28:50 -0300

    aj06

--- js/function.js
+++ js/function.js
@@ -13,15 +13,15 @@ window.onload = () => {
     const boaNoite_text = document.createTextNode('Boa noite!')
 
     if(hora <= '12'){
-        banner.style.backgroundImage = "url('/assets/img-manha.jpg')"
+        banner.style.backgroundImage = "url('../assets/img-manha.jpg')"
         welcome.appendChild(bomDia_text)
         banner_call.style.background = '#27A5A640'
     }else if (hora <= '18'){
-        banner.style.backgroundImage = "url('/assets/img-tarde.jpg')"
+        banner.style.backgroundImage = "url('../assets/img-tarde.jpg')"
         welcome.appendChild(boaTarde_text)
         banner_call.style.background = '#378679a6'
     }else{
-        banner.style.backgroundImage = "url('/assets/img-noite.jpg')"
+        banner.style.backgroundImage = "url('../assets/img-noite.jpg')"
         welcome.appendChild(boaNoite_text)
         banner_call.style.background = '#1d1d1d80'
     }
